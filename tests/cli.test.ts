e65bb27d1d7b1c4586e0bab7dd5703b62e26e09a
Mgen <m@mgenware.com>
2019-12-24 18:57:01 +0800

    Extract some common testing utils

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,14 +1,4 @@
-import * as assert from 'assert';
-import * as tw from 'temp-write';
-import * as mfs from 'm-fs';
-import * as util from 'util';
-import { exec } from 'child_process';
-import * as helper from './helper';
-const execAsync = util.promisify(exec);
-
-function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+import { testCLI } from './helper';
 
 it('Default mode', async () => {
   const src = `import { html, customElement, property } from 'lit-element';
@@ -19,13 +9,7 @@ declare global {
         "foo-bar": FooBar;//comment
     }
 }`;
-  const tmpFile = await tw(src);
-  await execAsync(`node ./dist/main.js "${tmpFile}"`);
-  await sleep(50);
-  const fileContents = await mfs.readTextFileAsync(tmpFile);
-  assert.equal(
-    fileContents,
-    `import { html, customElement, property } from 'lit-element';
+  const expected = `import { html, customElement, property } from 'lit-element';
 @customElement('my-element')
 export class MyElement extends LitElement {
 }
@@ -35,8 +19,8 @@ declare global {
         "my-element": MyElement;
     }
 }
-`,
-  );
+`;
+  await testCLI(src, expected);
 });
 
 it('Default mode + Prettier', async () => {
@@ -48,17 +32,7 @@ declare global {
         "foo-bar": FooBar;//comment
     }
 }`;
-
-  const srcFile = await tw(src);
-  const ptConfigFile = await helper.prettierConfigFile();
-  await execAsync(
-    `node ./dist/main.js "${srcFile}" --prettier "${ptConfigFile}"`,
-  );
-  await sleep(50);
-  const fileContents = await mfs.readTextFileAsync(srcFile);
-  assert.equal(
-    fileContents,
-    `import { html, customElement, property } from 'lit-element';
+  const expected = `import { html, customElement, property } from 'lit-element';
 @customElement('my-element')
 export class MyElement extends LitElement {}
 declare global {
@@ -67,6 +41,6 @@ declare global {
     'my-element': MyElement;
   }
 }
-`,
-  );
+`;
+  await testCLI(src, expected, { prettier: true });
 });
--- tests/helper.ts
+++ tests/helper.ts
@@ -1,4 +1,19 @@
+import * as assert from 'assert';
 import * as tw from 'temp-write';
+import * as mfs from 'm-fs';
+import * as util from 'util';
+import { exec } from 'child_process';
+import * as helper from './helper';
+const execAsync = util.promisify(exec);
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export interface TestCLIOption {
+  prettier?: boolean;
+  safeMode?: boolean;
+}
 
 export async function prettierConfigFile(): Promise<string> {
   const config = `module.exports = {
@@ -7,4 +22,29 @@ export async function prettierConfigFile(): Promise<string> {
     endOfLine: 'lf',
   };`;
   return await tw(config, '.prettier.js');
+}
+
+export async function testCLI(
+  src: string,
+  expected: string,
+  options?: TestCLIOption,
+) {
+  const srcFile = await tw(src);
+  let ptConfigFile: string | undefined;
+  options = options || {};
+  if (options.prettier) {
+    ptConfigFile = await helper.prettierConfigFile();
+  }
+
+  let cmd = `node ./dist/main.js "${srcFile}"`;
+  if (options.prettier) {
+    cmd += ` --prettier "${ptConfigFile}"`;
+  }
+  if (options.safeMode) {
+    cmd += ` --safe-mode`;
+  }
+  await execAsync(cmd);
+  await sleep(50);
+  const fileContents = await mfs.readTextFileAsync(srcFile);
+  assert.equal(fileContents, expected);
 }
